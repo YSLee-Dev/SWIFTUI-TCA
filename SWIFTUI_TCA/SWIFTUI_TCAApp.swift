f5173557ff11611493d5a326bf4baef8e2427700
YSLee-Dev <ysleedev01@gmail.com>
2024-05-23 17:06:34 +0900

    First Init

---
+++ SWIFTUI_TCA/SWIFTUI_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_TCAApp.swift
+//  SWIFTUI_TCA
+//
+//  Created by 이윤수 on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
