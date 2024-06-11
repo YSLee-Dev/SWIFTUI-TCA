4066428f9e28e660d7addc3140dcf32aa377b63a
YSLee-Dev <ysleedev01@gmail.com>
2024-06-11 16:33:53 +0900

    Feat(ResultLoad): query 값을 받아서 섞은 후 Return 하는 Manager 추가

---
+++ SWIFTUI_TCA/Dependency/ResultLoadManager.swift
@@ -0,0 +1,21 @@
+//
+//  ResultLoadManager.swift
+//  SWIFTUI_TCA
+//
+//  Created by 이윤수 on 6/11/24.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct ResultLoadManager: ResultLoadProtocol {
+    func querySearch(query: String) -> [String] {
+        [
+            String(query.shuffled()),
+            String(query.shuffled()),
+            String(query.shuffled()),
+            String(query.shuffled()),
+            String(query.shuffled())
+        ]
+    }
+}
---
+++ SWIFTUI_TCA/Dependency/ResultLoadProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ResultLoadProtocol.swift
+//  SWIFTUI_TCA
+//
+//  Created by 이윤수 on 6/11/24.
+//
+
+import Foundation
+
+protocol ResultLoadProtocol {
+    func querySearch(query: String) -> [String]
+}
