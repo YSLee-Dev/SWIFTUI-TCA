ce72715f69b9d00fa574a0bb5aca56c78a0102f5
YSLee-Dev <ysleedev01@gmail.com>
2024-07-02 15:43:48 +0900

    Feat(MultiStore): forEach, ifCaseLet 등 다양한 MultiStore 방식 학습

--- SWIFTUI_TCA/TCA_ Grammar_5.playground/Contents.swift
+++ SWIFTUI_TCA/TCA_ Grammar_5.playground/Contents.swift
@@ -33,3 +33,30 @@ import SwiftUI
 /// - 내부에는 store가 nil 일 때를 감지하여 nil 값을 무시하며, nil 값에 따라 View를 다르게 리턴함
 /// + 내부에서 리턴하는 View는 이미 Store와 withViewStore를 통해 연결되어 있음
 ///
+/// forEach
+/// - 자식의 State가 컬렉션 타입일 때 forEach를 통해서 부모 로직과 자식 로직을 수행할 수 있게 함
+/// -> 부모 State에 자식 State가 컬렉션 타입일 때 사용할 수 있음
+/// - ID를 사용하여 안전하게 접근하기 위해 IdentifiedArray 타입을 사용함
+/// - forEach도 동일하게 자식 -> 부모 순서로 호출됨
+/// - 매개변수로는 자식의 State, Action을 keypath, casepath로 전달하며, 클로저로 각 reducer를 return 해야함
+///
+/// IdentifiedArray?
+/// - 객체의 컬렉션 타입을 처리할 때 사용하며, 각 객체들이 고유한 식별자를 가지고 있어야 할 때 사용함
+/// - 내부 타입은 Identifiable 프로토콜을 채택해야 하며, 채택한 타입은 "id"라는 고유 식별자를 제공해야함
+/// -  Identifiable<Type>을 통해 정의
+///
+/// forEachStore
+/// - 배열된 State를 순회하며, 각 항목에 맞는 View를 생성하는데 사용됨
+/// -> 자녀의 State를 순회하며 View를 생성함
+/// - 생성자로 scope를 통해 부모 store를 분리하여 자녀 store를 넣어야하며, 각 고유 ID를 넣어야함
+/// - 클로저로 scope된 store를 제공하며, View를 return 해야함
+///
+/// ifCaseLet
+/// - 부모 State가 Enum 타입이고, 자식이 State를 가질 때 특정 케이스에 대해 자식 Reducer를 실행할 때 사용
+/// -> 자식 Action이 발생하면, 특정 자식의 Reducer를 실행하는 것
+/// - ifCaseLet도 동일하게 자식 -> 부모 순서로 호출됨
+///
+/// SwitchStore
+/// - ifCaseLet과 동일하게 State가 Enum 타입으로 되어있을 때 Case에 따라 View를 표출할 수 있음
+/// - Swtich를 통해서 자식을 나눈 경우 caseLet을 통해 특정 자식의 State/Action만 처리하는 Store를 만든 후 클로저로 View를 리턴함 (Scope와 비슷하지만 다름)
+/// - 단, 1.7 버전 이상부터는 if let 구문으로 사용
