03424b6f7ee4bec34447141036ecf899a82f2dd8
YSLee-Dev <ysleedev01@gmail.com>
2024-07-17 15:29:23 +0900

    Feat(SearchView): 2depth로 나누는 작업: TwoStep View, Reducer 추가

---
+++ SWIFTUI_TCA/SearchView/SearchTwoStepFeature.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTwoStepFeature.swift
+//  SWIFTUI_TCA
+//
+//  Created by 이윤수 on 7/17/24.
+//
+
+import SwiftUI
+
+struct SearchTwoStepFeature: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchTwoStepFeature()
+}
---
+++ SWIFTUI_TCA/SearchView/SearchTwoStepView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewTwoStep.swift
+//  SWIFTUI_TCA
+//
+//  Created by 이윤수 on 7/17/24.
+//
+
+import SwiftUI
+
+struct SearchViewTwoStep: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchViewTwoStep()
+}
