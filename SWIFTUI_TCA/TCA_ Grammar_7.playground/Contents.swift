9cc8d14fada65800134c178a88bd99e63df4319b
YSLee-Dev <ysleedev01@gmail.com>
2024-07-18 15:37:34 +0900

    Feat(Etc): ObservableState 학습

--- SWIFTUI_TCA/TCA_ Grammar_7.playground/Contents.swift
+++ SWIFTUI_TCA/TCA_ Grammar_7.playground/Contents.swift
@@ -29,3 +29,16 @@ import SwiftUI
 ///
 /// TCA에서는 @Reducer를 통해 Reducer Struct이 Reducer 프로토콜을 채택하지 않아도, Reducer 기능을 수행할 수 있게 함
 /// + Action 등 Enum을 사용할 때 \.로 바로 접근이 가능하게 함
+///
+/// ObservableState
+/// - TCA 1.7 이하 버전에서는 Reducer를 정의 후 View에서 사용하려면, withViewStore를 통해 ViewStore 형태로 사용해야 했음
+/// -> ViewStore는 Store를 감싼 형태로 View와 Store를 연결해주며, State 변화 시 View를 업데이트 시킴
+/// --> 실제로 ViewStore는 @ObservedObject를 준수하고 있음
+///
+/// TCA에서는 컴파일러 성능을 개선 시키면서 withViewStore 대신 viewStore를 따로 생성하여, @ObservedObject로 관리하라고 추천함
+/// - withViewStore는 View가 복잡해질 경우 성능 이슈가 있음
+/// - 하지만, View마다 ViewStore를 생성할 경우 보일러플레이트 코드가 심해짐
+///
+/// ObservableState는 매크로로, Reducer 내부 State에 붙일 경우 State를 Observable하게 함
+/// - ViewStore, withViewStore를 작업하지 않고도 바로 View에서 사용이 가능함
+/// -> 단, iOS17부터만 바로 사용 가능하며, iOS16 이하는 withPerceptionTracking{}로 감싼 뷰를 사용해야함
